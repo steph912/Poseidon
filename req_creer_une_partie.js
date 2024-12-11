36913826fa5eb6f5d92f29a920b529d6d163612e
tommy zheng <tozheng@et.esiea.fr>
2024-12-11 09:06:04 +0100

    fait la page cree une partie

--- req_creer_une_partie.js
+++ req_creer_une_partie.js
@@ -1,1 +1,62 @@
 // creer une partie + ensuite rediriger sur attente_du_joueur.html
+"use strict";
+
+const fs = require("fs");
+const nunjucks = require("nunjucks");;
+
+const trait = function (req, res, query) {
+
+	let marqueurs;
+	let page;
+	let contenu_fichier;
+	let listeMembres;
+	let trouve;
+	
+	//GENERE UN NOMBRE DE 000 000 A 999 999 
+	function codeAleatoire() {
+		const caracteres = "0123456789";
+		const tailleCode = 6;
+		let code = "";
+	
+		for (let i = 0; i < tailleCode; i++) {
+			const indexAleatoire = Math.floor(Math.random() * caracteres.length);
+			code += caracteres[indexAleatoire];
+		}
+	
+		return code;
+	}
+	
+	console.log("Votre code généré est : " + codeAleatoire());
+
+	// ON LIT LES PARTIES EXISTANTS
+
+	contenu_fichier = fs.readFileSync("partie.json", 'utf-8');
+	listeMembres = JSON.parse(contenu_fichier);
+
+	if (trouve === true) {
+    // SI CREATION PAS OK, ON REAFFICHE PAGE FORMULAIRE AVEC ERREUR
+
+		page = fs.readFileSync('jouer_contre_un_joueur.html', 'utf-8');
+       
+		marqueurs = {};
+		marqueurs.erreur = "ERREUR, la partie existe deja : ";
+		marqueurs.pseudo = query.pseudo;
+		page = nunjucks.renderString(page, marqueurs);
+
+	} else {
+	// SI CREATION OK, ON RENVOIE SUR LA PAGE ATTENTE DU JOUEUR 
+		page = fs.readFileSync('attente_du_joueur.html', 'UTF-8');
+
+		marqueurs = {};
+		marqueurs.pseudo = query.pseudo;
+		marqueurs.password = query.password;
+		page = nunjucks.renderString(page, marqueurs);
+	}
+	
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write(page);
+	res.end();
+};
+
+module.exports = trait;
+
